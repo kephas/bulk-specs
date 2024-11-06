c2dc8d41d9694c86e21a7c7e20e562747758b1d7
Pierre Thierry <pierre@nothos.net>
2024-11-06 22:43:12 +0100

    Update Nix

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/23.11.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz") {} }:
 
 pkgs.mkShell {
   packages = [ pkgs.entr pkgs.xml2rfc pkgs.gnumake ];
